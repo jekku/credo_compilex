25456243774e2cebe6a772e10ba7bc77dbcd52c8
Jekri Preclaro Orlina <jekripreclaroorlina@gmail.com>
2022-09-01 13:32:36 +0100

    Initialize

---
+++ lib/credo_compilex.ex
@@ -0,0 +1,18 @@
+defmodule CredoCompilex do
+  @moduledoc """
+  Documentation for `CredoCompilex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CredoCompilex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/credo_compilex_test.exs
@@ -0,0 +1,8 @@
+defmodule CredoCompilexTest do
+  use ExUnit.Case
+  doctest CredoCompilex
+
+  test "greets the world" do
+    assert CredoCompilex.hello() == :world
+  end
+end
